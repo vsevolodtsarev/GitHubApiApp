52ad4d7ffa5d003bb7ee734a70cad927a2b0b7b2
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-29 21:33:10 +0300

    Add FollowersListViewController

---
+++ GitHubApiApp/FollowersListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FollowersListViewController.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 29.01.2024.
+//
+
+import UIKit
+
+class FollowersListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
