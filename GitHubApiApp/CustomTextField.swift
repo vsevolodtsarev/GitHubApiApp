ed17974e3a7044e5c28489da259b5b2aa5a2b3f0
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-29 19:47:00 +0300

    Add CustomTextField

--- GitHubApiApp/CustomTextField.swift
+++ GitHubApiApp/CustomTextField.swift
@@ -8,13 +8,31 @@
 import UIKit
 
 class CustomTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        minimumFontSize = 12
+        adjustsFontSizeToFitWidth = true
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
