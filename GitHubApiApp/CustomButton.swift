519fd253ebfa8576900572e21ce65fec63847c5b
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-29 19:45:38 +0300

    Add CustomButton

---
+++ GitHubApiApp/CustomButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomButton.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 29.01.2024.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GitHubApiApp/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 29.01.2024.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
