a3bdcf340c0dc8c546658b4d34ed7166c1daaf3d
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-29 19:46:40 +0300

    Add CustomButton

--- GitHubApiApp/CustomButton.swift
+++ GitHubApiApp/CustomButton.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class CustomButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
