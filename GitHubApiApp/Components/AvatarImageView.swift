8b86833763a02e3d035e621cf5e6f1051342f185
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-30 16:08:28 +0300

    Create FollowerCell + AvatarImageView for it

---
+++ GitHubApiApp/Components/AvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  AvatarImageView.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 30.01.2024.
+//
+
+import UIKit
+
+final class AvatarImageView: UIImageView {
+    
+    private let placeHolderImage = UIImage(resource: .avatarPlaceholder)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeHolderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
