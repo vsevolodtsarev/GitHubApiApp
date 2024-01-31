5f9bea2b78f7afece77a7ece2f26e3baaecdd3dd
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-31 11:41:00 +0300

    Add CollectionView and DiffableDataSource for it

--- GitHubApiApp/Screens/FollowersListViewController.swift
+++ GitHubApiApp/Screens/FollowersListViewController.swift
@@ -9,7 +9,14 @@ import UIKit
 
 final class FollowersListViewController: UIViewController {
     
+    private enum Section {
+        case main
+    }
+    
     private let username: String
+    private var collectionView: UICollectionView?
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Follower>?
+    private var followers: [Follower] = []
     
     init(username: String) {
         
@@ -23,17 +30,75 @@ final class FollowersListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        guard let collectionView = collectionView else { return }
         
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    private func configureDataSource() {
+        guard let collectionView = collectionView else { return }
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, follower in
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as? FollowerCell
+                cell?.set(follower: follower)
+                return cell
+            })
+    }
+    
+    private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    private func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            
+            self.dataSource?.apply(snapshot, animatingDifferences: true)
+        }
+    }
+    
+    private func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result  in
             
-            guard let self else { return }
+            guard let self = self else { return }
             
             switch result {
             case .success(let followers):
-                print("followers count = \(followers.count)")
-                print(followers)
+                self.followers = followers
+                self.updateData()
                 
             case .failure(let error):
                 self.presentAlertViewControllerOnMainThread(alertTitle: "Bad Stuff Happened!",
@@ -41,10 +106,5 @@ final class FollowersListViewController: UIViewController {
                                                             buttonTitle: "Ok")
             }
         }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
