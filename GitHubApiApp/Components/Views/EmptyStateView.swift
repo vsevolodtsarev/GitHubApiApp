fa9e1df2b3fc0529aa91ad31cc326c9ee0dce3c2
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-01 12:05:57 +0300

    Add EmptyStateView

---
+++ GitHubApiApp/Components/Views/EmptyStateView.swift
@@ -0,0 +1,52 @@
+//
+//  EmptyStateView.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 01.02.2024.
+//
+
+import UIKit
+
+final class EmptyStateView: UIView {
+    
+    private let logoImageView = UIImageView()
+    private let messageLabel = CustomTitleLabel(textAlignment: .center, fontSize: 20)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(message: String) {
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+    }
+    
+    private func configure() {
+        addSubview(logoImageView)
+        addSubview(messageLabel)
+        
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
+        
+        logoImageView.image = .emptyStateLogo
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+        ])
+    }
+}
