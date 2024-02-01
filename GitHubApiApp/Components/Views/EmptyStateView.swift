57f1550a64d1bdeff58bdabd76a9bd478fe0e9a4
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-01 13:03:42 +0300

    Change constraint

--- GitHubApiApp/Components/Views/EmptyStateView.swift
+++ GitHubApiApp/Components/Views/EmptyStateView.swift
@@ -47,7 +47,7 @@ final class EmptyStateView: UIView {
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1.3),
             logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 400)
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 370)
         ])
     }
 }
