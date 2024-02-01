2f6d635c39117bbb7535cdd14efe33e3544792fd
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-01 11:01:08 +0300

    Add activity Indicator

--- GitHubApiApp/Screens/FollowersListViewController.swift
+++ GitHubApiApp/Screens/FollowersListViewController.swift
@@ -94,10 +94,11 @@ final class FollowersListViewController: UIViewController {
     }
     
     private func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result  in
             
             guard let self = self else { return }
-            
+            self.dismissLoadingView()
             switch result {
             case .success(let followers):
                 if followers.count < 100 { hasMoreFollowers = false }
