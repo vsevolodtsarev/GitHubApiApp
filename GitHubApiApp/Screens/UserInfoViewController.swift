367a93c00db47b5d3cbab8528687be52e2772c4e
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-01 19:19:05 +0300

    Test getUserInfo

--- GitHubApiApp/Screens/UserInfoViewController.swift
+++ GitHubApiApp/Screens/UserInfoViewController.swift
@@ -26,7 +26,17 @@ final class UserInfoViewController: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
         navigationItem.rightBarButtonItem = doneButton
-        print(username)
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                presentAlertViewControllerOnMainThread(alertTitle: "Something wrong", alertMessage: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
     
     @objc func dismissViewController() {
