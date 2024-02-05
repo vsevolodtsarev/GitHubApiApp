a39e03e42ceb97ddb500ab048d24feb527c77c24
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-05 10:34:47 +0300

    Add childViews: FollowerItemViewController, RepoItemViewController

---
+++ GitHubApiApp/Components/Views/ItemInfoViewControllers/FollowerItemViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FollowerItemViewController.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 05.02.2024.
+//
+
+import UIKit
+
+final class FollowerItemViewController: ItemInfoViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ GitHubApiApp/Components/Views/ItemInfoViewControllers/RepoItemViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RepoItemViewController.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 05.02.2024.
+//
+
+import UIKit
+
+final class RepoItemViewController: ItemInfoViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
