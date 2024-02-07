aedf716857f9194d35a6c5d3955b250997f26960
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-07 10:13:36 +0300

    Finish FavoritesViewController

--- GitHubApiApp/Screens/FavoritesViewController.swift
+++ GitHubApiApp/Screens/FavoritesViewController.swift
@@ -8,18 +8,105 @@
 import UIKit
 
 final class FavoritesViewController: UIViewController {
-
+    
+    private let tableView = UITableView()
+    private var favorites: [Follower] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    private func configureViewController() {
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Favorites"
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
         
-        PersistenceManager.retrieveFavorites { result in
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(FavoritesCell.self, forCellReuseIdentifier: FavoritesCell.reuseID)
+    }
+    
+    private func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else { return }
             switch result {
             case .success(let favorites):
-                print(favorites)
+                if favorites.isEmpty {
+                    showEmptyStateView(with: "No favorites?\nAdd one on the follower screen.", in: view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+            
             case .failure(let error):
-                print(error)
+                presentAlertViewControllerOnMainThread(alertTitle: "Something went wrong",
+                                                       alertMessage: error.rawValue,
+                                                       buttonTitle: "Ok")
             }
         }
     }
+}
+
+extension FavoritesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let destinationViewController = FollowersListViewController(username: favorite.login)
+        destinationViewController.title = favorite.login
+        navigationController?.pushViewController(destinationViewController, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        if favorites.isEmpty {
+            showEmptyStateView(with: "No favorites?\nAdd one on the follower screen.", in: view)
+        }
+        
+        PersistenceManager.updateWith(favorite: favorite,
+                                      actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            
+            presentAlertViewControllerOnMainThread(alertTitle: "Unable to remove",
+                                                   alertMessage: error.rawValue,
+                                                   buttonTitle: "Ok")
+        }
+    }
+}
+
+extension FavoritesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesCell.reuseID) as? FavoritesCell
+        else {
+            return UITableViewCell() }
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+    
 }
