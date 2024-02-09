680b2782a048cb4da02276233c5c0b64d531615c
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-09 08:20:17 +0300

    Improve code style with init for custom views

--- GitHubApiApp/Components/Views/EmptyStateView.swift
+++ GitHubApiApp/Components/Views/EmptyStateView.swift
@@ -21,10 +21,9 @@ final class EmptyStateView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(message: String) {
-        super.init(frame: .zero)
+    convenience init(message: String) {
+        self.init(frame: .zero)
         messageLabel.text = message
-        configure()
     }
     
     private func configure() {
