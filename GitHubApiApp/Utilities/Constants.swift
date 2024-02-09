b6218750dd82bbda5946f6fb6f44a08af23bb8a7
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-09 09:25:24 +0300

    Add constants

---
+++ GitHubApiApp/Utilities/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 09.02.2024.
+//
+
+import UIKit
+
+enum Urls {
+    static let gitHubBaseUrl = "https://api.github.com"
+}
+
+enum SFSymbolsImage {
+    static let location = UIImage(systemName: "mappin.and.ellipse")
+    static let repos = UIImage(systemName: "folder")
+    static let gists = UIImage(systemName: "text.alignleft")
+    static let followers = UIImage(systemName: "heart")
+    static let following = UIImage(systemName: "person.2")
+}
