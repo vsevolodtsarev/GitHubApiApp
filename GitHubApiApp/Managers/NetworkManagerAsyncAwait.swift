e1131b04100a94a718f896f9c9252c0eae7b9f94
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-11 16:03:40 +0300

    Add new network manager

---
+++ GitHubApiApp/Managers/NetworkManagerAsyncAwait.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManagerAsync:Await.swift
+//  GitHubApiApp
+//
+//  Created by Всеволод Царев on 11.02.2024.
+//
+
+import Foundation
+
+final class NetworkManagerAsyncAwait {
+    
+}
